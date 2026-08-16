652714a58b046c113ff9633aa7dfe7f9985bb868
Izaaz <amirizaaz013@gmail.com>
2026-08-16 17:47:24 +0800

    Rename Duke to Aigis. Add exit message.

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Basic, has taken Data Structures and Algorithms and Intro to C
+* IDE and level of expertise: Minimal, has used Visual Studio on occasion
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Aigis.java
@@ -0,0 +1,16 @@
+public class Aigis {
+    public static void main(String[] args) {
+        String banner = "     A       IIIII   GGGG    IIIII   SSSSS\n"
+                + "    A A        I     G        I     S    \n"
+                + "   AAAAA       I     G  GG    I      SSSS\n"
+                + "  A     A      I     G   G    I          S\n"
+                + " A       A   IIIII   GGGG   IIIII   SSSSS\n"
+                + "\n"
+                + "Aigis is ready to help!\n";
+        String closing = " ----------------------------------------------------\n"
+                + "Tasks completed. See you again soon!\n"
+                + "__________________________________________________________\n";
+        System.out.println(banner);
+        System.out.println(closing);
+    }
+}
